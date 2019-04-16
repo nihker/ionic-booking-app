4f0d900a7df92bd4ed9db78ab3cc4dc7c43c7471
nihker <nihadkeric@gmail.com>
2019-04-16 12:20:22 +0200

    Added button for book the place (with back func)

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
+  onBookPlace(){
+    // Navigation with Router from angualr/router
+    //this.router.navigateByUrl('places/tabs/discover');
+    //this.router.navigate(['places', 'tabs', 'discover']);
+
+    // Navigation with NavController from ionic/router (with animation)
+    this.navCtrl.navigateBack('/places/tabs/discover');
+  }
+ 
 }

f9a0c227bd6bd52f33c904db8e01ca56e0f82609
nihker <nihadkeric@gmail.com>
2019-04-24 08:14:03 +0200

    Create modal with component crete-booking

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { NavController, ModalController } from "@ionic/angular";
+import { CreateBookingComponent } from "../../../bookings/create-booking/create-booking.component";
 
 @Component({
-  selector: 'app-place-detail',
-  templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  selector: "app-place-detail",
+  templateUrl: "./place-detail.page.html",
+  styleUrls: ["./place-detail.page.scss"]
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController
+  ) {}
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
-  onBookPlace(){
+  onBookPlace() {
     // Navigation with Router from angualr/router
     //this.router.navigateByUrl('places/tabs/discover');
     //this.router.navigate(['places', 'tabs', 'discover']);
-
     // Navigation with NavController from ionic/router (with animation)
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    //this.navCtrl.navigateBack('/places/tabs/discover');
+    this.modalCtrl.create({component: CreateBookingComponent}).then(modalEl => {
+      modalEl.present();
+    })
   }
- 
 }

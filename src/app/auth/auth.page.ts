ae5254a118db50e245df0675f156ff3d12230c70
nihker <nihadkeric@gmail.com>
2019-05-09 07:46:03 +0200

    Added onSubmit logic for login and check the form mode

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,33 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { LoadingController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  selector: "app-auth",
+  templateUrl: "./auth.page.html",
+  styleUrls: ["./auth.page.scss"]
 })
 export class AuthPage implements OnInit {
   isLogin = true;
 
-  constructor(private authService: AuthService, private router: Router, loadingCtrl: LoadingController) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    loadingCtrl: LoadingController
+  ) {}
 
-  ngOnInit() {
-  }
-
-  onLogin() {
-    this.authService.login();
-    this.router.navigateByUrl('/places');
-  }
+  ngOnInit() {}
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login server
+      this.authService.login();
+      this.router.navigateByUrl("/places");
+    } else {
+      // Send a request to signup server
+    }
   }
 
-  onSwitchAuthMode(){
+  onSwitchAuthMode() {
     this.isLogin = !this.isLogin;
   }
-
 }
